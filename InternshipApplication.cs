348d784df7d72b72f4dbc5f711889dfa0f503426
Sam Portis <126818488+SamPortis@users.noreply.github.com>
2025-09-25 00:23:31 -0700

    Add files via upload
    The data part of my internship tracker housing all of my classes and database context

---
+++ InternshipApplication.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipTracker.Data
+{
+    public class InternshipApplication : BaseDomainEntity
+    {
+        public InternshipApplication()
+        {
+            Company = new Company();
+        }
+
+        public int CompanyId { get; set; } //foreign key to Company
+
+        public virtual Company? Company { get; set; } //navigation property to Company
+
+        [Required]
+        public int StatusId { get; set; }
+
+        public Status? Status { get; set; }
+
+        [Required]
+        public string? Position { get; set; }
+
+        public string? Notes { get; set; }
+
+        public bool NeedToTakeOA { get; set; }
+
+    }
+}
